bea554499ddcff163d223dd6e539adb720fa3d0c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 20:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function(i){
-    return {firstName: `i.split(' ')[0]`, lastName: `i.split(' ')[1]`}
+    return {firstName: `${i.split(' ')[0]}`, lastName: `${i.split(' ')[1]}`}
   })
 }
