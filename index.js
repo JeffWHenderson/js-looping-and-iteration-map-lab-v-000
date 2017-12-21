56bb370657588f2bbf05b69880cdc210d2b00c61
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 20:05:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(array) {
   array.map(function(i){
-    `${i.name} is from ${i.hometown}`
+    return `${i.name} is from ${i.hometown}`
   })
 }
